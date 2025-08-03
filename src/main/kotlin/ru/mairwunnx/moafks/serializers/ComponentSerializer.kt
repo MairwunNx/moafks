01c90cddd8e21799bec6929b0f8a0e632e890fdd
Pavel Erokhin <mairwunnx@gmail.com>
2025-08-03 11:16:59 +0300

    Заменил MiniMessage на PaperAudience для сериализации компонентов

--- src/main/kotlin/ru/mairwunnx/moafks/serializers/ComponentSerializer.kt
+++ src/main/kotlin/ru/mairwunnx/moafks/serializers/ComponentSerializer.kt
@@ -6,10 +6,9 @@ import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import net.kyori.adventure.text.Component
-import net.kyori.adventure.text.minimessage.MiniMessage
+import ru.mairwunnx.moafks.platform.PaperAudience.mm
 
 object ComponentSerializer : KSerializer<Component> {
-  private val mm = MiniMessage.miniMessage()
   override val descriptor = PrimitiveSerialDescriptor("Component", PrimitiveKind.STRING)
 
   override fun serialize(encoder: Encoder, value: Component) = encoder.encodeString(mm.serialize(value))
